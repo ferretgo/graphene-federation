100143619197938614d8bfee7d3886135b72b186
Andrii Gakhov <gakhov@users.noreply.github.com>
2020-11-24 16:56:01 +0100

    Specify auto_camelcase as an input parameter
    schema has no such attribute in v3

--- graphene_federation/main.py
+++ graphene_federation/main.py
@@ -4,9 +4,9 @@ from .entity import get_entity_query, register_entity
 from .service import get_service_query
 
 
-def _get_query(schema, query_cls=None):
+def _get_query(schema, query_cls=None, auto_camelcase=True):
     bases = [get_service_query(schema)]
-    entity_cls = get_entity_query(schema.auto_camelcase)
+    entity_cls = get_entity_query(auto_camelcase)
     if entity_cls:
         bases.append(entity_cls)
     if query_cls is not None:
@@ -17,5 +17,6 @@ def _get_query(schema, query_cls=None):
 
 
 def build_schema(query=None, mutation=None, **kwargs):
-    schema = graphene.Schema(query=query, mutation=mutation, **kwargs)
-    return graphene.Schema(query=_get_query(schema, query), mutation=mutation, **kwargs)
+    auto_camelcase = kwargs.pop("auto_camelcase", True)
+    schema = graphene.Schema(query=query, mutation=mutation, auto_camelcase=auto_camelcase, **kwargs)
+    return graphene.Schema(query=_get_query(schema, query, auto_camelcase=auto_camelcase), mutation=mutation, **kwargs)
